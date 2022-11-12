1584167625b34b9470457b5de65681207250e6b4
Jalpa Patel <pjalpa015@gmail.com>
2022-11-12 12:52:46 -0500

    Adding Tax Forms

--- src/tasks/task-2-web-application/main.py
+++ src/tasks/task-2-web-application/main.py
@@ -1,18 +1,22 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
-
+# main method renders the Tax form
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def main():
+    return render_template("tax-form.html")
 
-
-@app.route('/tax-direction/')
-@app.route('/tax-direction/<name>')
+#tax_direction method takes input from the form submit and shows the results page
+@app.route('/tax-direction/', methods=['POST', 'GET'])
 def tax_direction(name=None):
-    return render_template('tax-form.html', name=name)
+    if request.method == 'POST':
+        result = request.form
+        json_result = dict(result)
+        print(json_result)
+        return render_template("results.html", result=result)
 
 
 if __name__ == '__main__':
